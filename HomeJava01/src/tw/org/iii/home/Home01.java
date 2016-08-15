0b15f5cde1819d5ac85aec10f248994d8041dc85
ricky0624 <ricky23750719@gmail.com>
2016-08-16 00:01:57 +0800

    Test Again And Again
    that all

--- HomeJava01/src/tw/org/iii/home/Home01.java
+++ HomeJava01/src/tw/org/iii/home/Home01.java
@@ -3,7 +3,7 @@ package tw.org.iii.home;
 public class Home01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,world! world hello!");
+		System.out.println("Hello,world!");
 
 	}
 
